52050ef6d228409d2316969436d2e22a4ebbca0d
欧思维 <ousiwei@rong360-MacBook87.local>
2018-07-23 16:10:48 +0800

    version2.1 animation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="page_fade" appear> <!--加页面切换动画和初始渲染动画-->
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -21,5 +23,23 @@ html{
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.page_fade, .appear {
+  position: absolute;
+  left: 0;
+  right: 0;
+}
+.page_fade-enter-active, .appear-enter-active {
+  transition: all 1.5s ease;
+}
+.page_fade-leave-active .appear-leave-active {
+  transition: all .3s cubic-bezier(2.0, 0.5, 0.8, 1.0);
+}
+.page_fade-enter, .page_fade-leave-to, .appear-enter, .appear-leave-to
+{
+  left: 0;
+  right: 0;
+  transform: translateX(100px);
+  opacity: 0;
 }
 </style>
